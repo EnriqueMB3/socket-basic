447bfbbad91d15f704eafa921ff88f277be11861
Enrique <enrique_.00@hotmail.com>
2018-08-21 18:04:11 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+
+const path = require('path');
+
+const app = express();
+let server = http.createServer(app);
+
+
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+//IO = conexion directa con el servidor
+module.exports.io = socketIO(server);
+require('./sockets/socket');
+
+
+//Escuchar el cliente
+
+
+
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
